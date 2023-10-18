9d0c39745b886bc0bacaf322516de30f4a9ce5fa
Raya Mohammed <rayam@hijrabank.local>
2023-10-18 15:54:06 +0300

    Task all

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 import sys
+
+
 def print_msg(dict_sc, total_file_size):
     """
     Method to print
@@ -9,10 +12,13 @@ def print_msg(dict_sc, total_file_size):
     Returns:
         Nothing
     """
+
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
+
+
 total_file_size = 0
 code = 0
 counter = 0
@@ -24,19 +30,25 @@ dict_sc = {"200": 0,
            "404": 0,
            "405": 0,
            "500": 0}
+
 try:
     for line in sys.stdin:
         parsed_line = line.split()  # ✄ trimming
         parsed_line = parsed_line[::-1]  # inverting
+
         if len(parsed_line) > 2:
             counter += 1
+
             if counter <= 10:
                 total_file_size += int(parsed_line[0])  # file size
                 code = parsed_line[1]  # status code
+
                 if (code in dict_sc.keys()):
                     dict_sc[code] += 1
+
             if (counter == 10):
                 print_msg(dict_sc, total_file_size)
                 counter = 0
+
 finally:
     print_msg(dict_sc, total_file_size)
